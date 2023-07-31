6ed6873ecd9e34118083903857b2adf2aa572e95
Deneth Perera <denethperera1998@gmail.com>
2023-07-31 09:13:32 +0530

    Complete task 4

---
+++ Demo4.java
@@ -0,0 +1,14 @@
+public class Demo4 {
+    public static void main(String[] args) {
+        int day = 28;
+        int month = 12;
+        int year = 2023;
+        int hour = 23;
+        int minute = 59;
+        int second = 59;
+
+
+        System.out.printf("\u001B[33m%s\u001B[0m/\u001B[32m%s\u001B[0m/\u001B[31m%s\u001B[0m \u001B[33m%s\u001B[0m:\u001B[34m%s\u001B[0m:\u001B[35m%s\u001B[0m \n", day,month,year,hour,minute,second);
+    }
+    
+}

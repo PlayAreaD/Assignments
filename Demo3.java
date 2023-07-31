56dc2f858c999a8476fe3d683f145a3cada70056
Deneth Perera <denethperera1998@gmail.com>
2023-07-31 07:52:26 +0530

    Complete task 3

---
+++ Demo3.java
@@ -0,0 +1,20 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        int areaCode = 123;
+        int partTwo = 456;
+        int partThree = 7890;
+
+        System.out.printf("\u001B[33m(%s)\u001B[0m %s-%s \n", areaCode,partTwo, partThree);
+        
+
+
+
+
+
+
+
+
+
+    }
+    
+}

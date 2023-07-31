155b473da4bab78431adda78dfa402771c01760a
Deneth Perera <denethperera1998@gmail.com>
2023-07-31 10:05:34 +0530

    Complete task 6a

---
+++ Demo6taska.java
@@ -0,0 +1,14 @@
+public class Demo6taska{
+    public static void main(String[] args) {
+        int numberOne =5;
+        int numberTwo =0;
+        String percentageMark = "%";
+
+        System.out.printf("\u001B[41m[    %s\u001B[0m\u001B[42m[%s%s   ]\u001B[0m \n", numberOne,numberTwo,percentageMark);
+        // int areaCode = 123;
+        // int partTwo = 456;
+        // int partThree = 7890;
+
+        // System.out.printf("\u001B[41m(%s)\u001B[0m %s-%s \n", areaCode,partTwo, partThree);
+    }
+}
